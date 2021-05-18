cddc3d04ac5e227aa202a6e83bb6788dcf15b587
Joshua <joshuamacleod@live.com>
2021-05-18 20:58:39 +0100

    backend and front end connection

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import Users from './components/users';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+    <div>
+      <Users />
+    </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
