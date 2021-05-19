90da08044c4c74892647d514f0d874244ea69cc8
Joshua <joshuamacleod@live.com>
2021-05-19 17:44:22 +0100

    login / signup settup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import './App.css';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from '@apollo/client'
 import Users from './components/users';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Landing from './components/Landing';
+import {setContext} from 'apollo-link-context'
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000",
-  cache: new InMemoryCache()
+const httpLink = new HttpLink({uri: 'http://localhost:4000'})
+const authLink = setContext(async(req, {headers}) => {
+  const token = localStorage.getItem('token')
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}`: null
+    }
+  }
 });
+
+
+const link = authLink.concat(httpLink as any)
+const client = new ApolloClient({
+  link: (link as any),
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
@@ -17,6 +34,12 @@ function App() {
       <Switch>
         <Route exact path="/">
           <Users />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
         </Route>
         <Route path="/landing">
           <Landing />
